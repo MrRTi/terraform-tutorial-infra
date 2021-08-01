96a748903def0761c05929793df626603649e461
Artem Musalitin <musalitin.a.a@gmail.com>
2021-08-01 13:47:50 +0000

    Add ssh to ingress rules

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ module "ec2_sg" {
   vpc_id      = data.aws_vpc.default.id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "https-443-tcp", "all-icmp"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp", "all-icmp", "ssh-tcp"]
   egress_rules        = ["all-all"]
 }
 
